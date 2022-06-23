a1a9450d9e5f0e4d7da272df4eb2ec7ae55ee305
vladverheij <vladverheij@gmail.com>
2022-06-23 18:51:50 +0200

    Added all files

---
+++ details.php
@@ -0,0 +1,39 @@
+<?php
+    require 'functions.php';
+    $connection = dbconnect();
+    
+    if( !isset( $_GET['id']) ){
+        echo "De ID is niet gezet";
+        exit;
+    }
+
+    $id = $_GET['id'];
+    $check_int = filter_var($id, FILTER_VALIDATE_INT);
+    if($check_int == false){
+        echo"Dit is geen getal";
+        exit;
+    }
+
+    $statement = $connection->prepare('SELECT * FROM `plaatsen` WHERE id=?');
+    $params = [$id];
+    $statement->execute($params);
+    $place = $statement->fetch(PDO::FETCH_ASSOC);
+
+    $result = $connection->query("SELECT * FROM `plaatsen`")
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css?v=<?php echo time()?>">
+    <title></title>
+</head>
+<body>
+
+<figure class="details_image" style="background-image: url(images/<?php echo $row['foto'];?>)"></figure>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+    require 'functions.php';
+    $connection = dbconnect();
+     
+    $result = $connection->query("SELECT * FROM `plaatsen`");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css?v=<?php echo time()?>">
+    <title>Tourist Highlights</title>
+</head>
+<body>
+<section>
+                <div class="container">
+                    <div class="row">
+                        <h1>The best travel spots to visit!</h1>
+                    <?php foreach($result as $row): ?>
+                        <div class="places-list__place">
+                            <h3><?php echo $row['titel']?></h3>
+                            <figure class="places-list__photo" style="background-image: url(images/<?php echo $row['foto'];?>)" > </figure>
+                            <p><?php echo $row['beschrijving'] ?></p>
+                            <a class="places-list__link" target="blanck" href="details.php?id=<?php echo $row['id'];?>">Meer info</a>
+                        </div>
+                        <?php endforeach;?>
+                    </div>
+                </div>
+            </section>
+</body>
+
+</html>
+details_image
