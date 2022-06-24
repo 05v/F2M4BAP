a0adf7ebf4cae650d15654d13ee1d3572493dd1f
vladverheij <vladverheij@gmail.com>
2022-06-24 13:14:42 +0200

    Opdracht 4

---
+++ Opdracht4/config.php
@@ -0,0 +1,12 @@
+<?php
+$config = [
+    'server' => 'localhost',
+    'database' => 'makeitrain',
+    'username' => 'root',
+    'password' => '',
+    'port' => '3306'
+];
+
+return $config;
+
+?>
